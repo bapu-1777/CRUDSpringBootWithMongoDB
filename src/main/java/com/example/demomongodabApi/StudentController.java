3f932a6524673ece34a67c4f259ac90b9613c298
bapu-1777 <mayankpv2000@gmail.com>
2022-03-28 18:30:57 +0530

    aggregation done

--- src/main/java/com/example/demomongodabApi/StudentController.java
+++ src/main/java/com/example/demomongodabApi/StudentController.java
@@ -2,14 +2,19 @@ package com.example.demomongodabApi;
 
 
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
@@ -18,7 +23,11 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import javax.annotation.PostConstruct;
 import javax.validation.Valid;
 import java.util.*;
 
@@ -29,6 +38,12 @@ public class StudentController {
 
     @Autowired
     private StudentRepo studentRepo;
+
+    String uri = "mongodb://localhost:27017";
+    @Autowired
+    MongoClient mongoClient = MongoClients.create(uri);
+//    @Autowired
+    MongoDatabase db = mongoClient.getDatabase("mayank");
 
     @Autowired
     MongoTemplate mongoTemplate;
@@ -41,6 +56,27 @@ public class StudentController {
         }else {
             return new ResponseEntity<>("NO Student available",HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping("/stname")
+    public ResponseEntity<?> byname(){
+        List<Student> bynam =studentRepo.findByStudent("Vekariya");
+        if (bynam.size()>0){
+            return new ResponseEntity<List<Student>>(bynam, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>("NO Student available",HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/function")
+    public void xyz(){
+        System.out.println(1);
+        studentRepo.usemauyank();
+        System.out.println(2);
+        studentRepo.loadserver();
+        System.out.println(3);
+        System.out.println(studentRepo.findBYFunction());
+        System.out.println(4);
     }
     @PostMapping("/student")
     public ResponseEntity<?> putStudent(@Valid @RequestBody Student student){
@@ -180,14 +216,66 @@ public class StudentController {
 
     @GetMapping("/mongo")
     public ResponseEntity<?> byjs(){
+//            db.eval("db.loadServerScripts();");
 
 
-            String uri = "mongodb://localhost:27017";
-            MongoClient mongoClient = MongoClients.create(uri);
-            MongoDatabase database = mongoClient.getDatabase("mayank");
+
+//        db.runCommand(new Document("$eval", "fullFillCollumns()"));
+//
+//        Document doc1 = db.runCommand(new Document("$eval", "echoFunction(5)"));
+//        System.out.println(doc1);
+//
+//        Document doc2 = db.runCommand(new Document("$eval", "myAddFunction(5,8)"));
+//        System.out.println(doc2);
+
+
 //            return database.doEval("addNumbers(3,4)").toString();
-        Document doc1 = database.runCommand(new Document("db.loadServerScripts()","addNumbers(4,5)"));
-        System.out.println(doc1);
+//
+//        Document doc1 = database.runCommand(new Document("use mayank","db.loadServerScripts()"));
+//        System.out.println(doc1);
+
+
+//        final BasicDBObject command = new BasicDBObject();
+//        command.put("$eval", "function() { return db.loadServerScripts(); }");
+//        command.put("$eval", "function() { return addNumbers(3); }");
+//        Document result = database.runCommand(command);
+
+//        final DBObject command = new BasicDBObject();
+//        command.put("$eval", "function() { return db." +"Student"+ ".find(); }");
+//        Document result = database.runCommand((Bson) command);
+//        System.out.println(result);
+
+
+
+//        MongoCollection<Document> gradesCollection = database.getCollection("Student");
+//        // find one document with new Document
+//        Document student1 = gradesCollection.find(new Document("lastName","Vekariya")).first();
+//        System.out.println("Student 1: " + student1.toJson());
+
+//        MongoClient mongoClient = MongoClients.create();
+//        MongoDatabase database = mongoClient.getDatabase("mayank");
+//        System.out.println("connected");
+//
+//
+//        Document buildInfoResults = database.runCommand(new Document("eval","addNumbers(4,5)"));
+//        System.out.println(buildInfoResults.toJson());
+//        System.out.println("load run");
+
+//        Document collStatsResults = database.runCommand(new Document("", "addNumbers(5,5)"));
+//        System.out.println(collStatsResults.toJson());
+//        System.out.println("done");
+
+//        MongoOperations mongoOperations;
+//        final BasicDBObject basicDBObject = new BasicDBObject();
+//        basicDBObject.put("eval", "function() { return db.Student.totalSize(); }");
+//        Document result = mongoOperations.executeCommand(basicDBObject);
+
+        String commandStr = "db.Student.totalSize()"; // or custom collection name in your case
+        Bson command = new BsonDocument(commandStr, new BsonInt64(1));
+        Document commandResult = db.runCommand(command);
+        System.out.println(commandResult);
+
+
         return null;
     }
 }
--- src/main/java/com/example/demomongodabApi/StudentRepo.java
+++ src/main/java/com/example/demomongodabApi/StudentRepo.java
@@ -1,16 +1,27 @@
 package com.example.demomongodabApi;
 
+import org.bson.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface StudentRepo extends MongoRepository<Student,String> {
 
-    @Query(value =  "{'lastName': ?0", fields = "")
-    Optional<Student> findByStudent(String student);
+    @Query(value =  "{'lastName': ?0}")
+    List<Student> findByStudent(String name);
+
+
+    @Query("use mayank")
+    void usemauyank();
+
+    @Query("db.loadServerScripts()")
+    void loadserver();
+
+    @Query("addNumbers(7,3)")
+    Document findBYFunction();
+
 
 }
